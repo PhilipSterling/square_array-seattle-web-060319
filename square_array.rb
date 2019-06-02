9fbdc8321ce9149a18caf58dbe15ae51aa690b59
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 03:38:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
-puts array[1]def square_array(array)
+def square_array(array)
   # your code here
   array.each do |n|
     text = n**2
     n = text
   end
 end
-array = [1,2,3]
-square_array(array)
